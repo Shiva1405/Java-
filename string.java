33020a00881be4300052af8b51fee1677932736a
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-05-06 11:51:25 +0530

    Update string.java

--- string.java
+++ string.java
@@ -8,5 +8,10 @@ public class string {
     System.out.println(s.substring(4,7)); // range lies between i and j-1.
     System.out.println(s.concat(m));// concat function conCATENATES TWO STRINGS 
     System.out.println(s.indexOf("world"));//return the index of the string 
+    StringBuffer sb = new StringBuffer("helloworld");
+    System.out.println(sb.length()); // prints the length of the string buffer
+    System.out.println(sb.capacity());// prints the total capicity of the string buffer 
+
 }
+
 }
