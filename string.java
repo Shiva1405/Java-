2eb98ce6f41eccd04a2f795fc46b1563c3a751b0
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-05-06 11:10:12 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args)
+{
+    String s = new string("helloworld");
+    System.out.println(s);
+    System.out.println(s.charAt(6));
+    
+}
+}
