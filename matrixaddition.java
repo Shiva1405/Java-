6230e8738fc416f56f54fe22767a029fbc327af9
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-05-06 11:46:49 +0530

    Update matrixaddition.java

--- matrixaddition.java
+++ matrixaddition.java
@@ -1,5 +1,5 @@
 public class matrixaddition{
-pu    public static void main(String args[])
+       public static void main(String args[])
 {
     int a[][]= {{1,2,3},{4,5,3},{1,2,3}};
     int b[][]={{1,2,3},{4,5,6},{7,8,9}};
