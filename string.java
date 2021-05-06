29b91a87b47c8e265523461b8f1d2b0fd1b6816b
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-05-06 11:37:56 +0530

    Update string.java

--- string.java
+++ string.java
@@ -1,12 +1,12 @@
 public class string {
     public static void main(String[] args)
 {
-    String s = new String("helloworld");
+    String s = new String("hello world bye");
     String m = new String("he");
     System.out.println(s);
     System.out.println(s.charAt(6)); // prints the character at the given index 
     System.out.println(s.substring(4,7)); // range lies between i and j-1.
-    System.out.println(s.concat(m));
-
+    System.out.println(s.concat(m));// concat function conCATENATES TWO STRINGS 
+    System.out.println(s.indexOf("world"));//return the index of the string 
 }
 }
