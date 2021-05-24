eab89883cb32db8c268a7634b62973d168378218
Shiva1405 <71342542+Shiva1405@users.noreply.github.com>
2021-05-24 11:59:06 +0530

    Create ifprogram.java

---
+++ ifprogram.java
@@ -0,0 +1,17 @@
+public class ifprogram {
+    public static void main(String args[]) {
+        int a=5,b=6,c=8;
+        if(c== a+b){
+            System.out.println("yes");
+
+        }
+        System.out.println("Bye");
+        if(true)
+        else
+        {
+            System.out.println("no");
+        }
+
+
+    }
+}
